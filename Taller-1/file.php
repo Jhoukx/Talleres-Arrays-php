60c19665c03bac98470c25958e1a73d6eab3162b
John González <jegonzalezesparragoza@gmail.com>
2023-06-05 10:11:07 -0500

    feat: :sparkles: Primer taller
    Recibe un número y retorna el nombre del planeta

--- Taller-1/file.php
+++ Taller-1/file.php
@@ -1,3 +1,9 @@
 <?php
-echo "HP";
+echo <<<HTML
+        <a href="index.html" target="_self">Volver</a><br/>
+    HTML;
+$sistemaSolar = array("Sol"=>1,"Mercurio"=>2,"Venus"=>3,"Tierra"=>4,"Marte"=>5,"Jupiter"=>6,"Saturno"=>7,"Urano"=>8,"Neptuno"=>9);
+$sistemaSolarR=array_flip($sistemaSolar);
+$numero = $_POST["nPlaneta"];
+echo "<h1>El planeta número $numero es: $sistemaSolarR[$numero]</h1>";
 ?>
